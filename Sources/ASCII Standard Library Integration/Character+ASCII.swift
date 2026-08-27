96f2adc85feaa01909328f508e511a35c1529022
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-27 19:56:10 +0200

    Fix build: Hashable on ASCII.Code, correct Case qualification

--- Sources/ASCII Standard Library Integration/Character+ASCII.swift
+++ Sources/ASCII Standard Library Integration/Character+ASCII.swift
@@ -47,7 +47,7 @@ extension Character.ASCII {
     }
 
     @inlinable
-    public func callAsFunction(case: ASCII.ASCII.Case) -> Character {
+    public func callAsFunction(case: ASCII.Case) -> Character {
         guard let byte = UInt8(ascii: character) else { return character }
         let converted = ASCII.Case.Conversion.convert(ASCII.Code(byte), to: `case`)
         return Character(UnicodeScalar(converted.underlying))
--- Sources/ASCII/ASCII.Code.swift
+++ Sources/ASCII/ASCII.Code.swift
@@ -1,7 +1,7 @@
 extension ASCII {
 
     @frozen
-    public struct Code: Sendable {
+    public struct Code: Sendable, Hashable {
 
         public let underlying: UInt8
 
